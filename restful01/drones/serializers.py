45d0d85e49adbcc9768dfb9f14c51389743e8250
Pedro <jpferreirafsa@gmail.com>
2024-09-22 21:57:59 -0300

    att

--- restful01/drones/serializers.py
+++ restful01/drones/serializers.py
@@ -14,7 +14,7 @@ class DroneSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Drone
-        fields = ('url', 'name', 'drone_category', 'manufacturing_date', 'has_it_completed', 'iserted_timestamp',)
+        fields = ('url', 'name', 'drone_category', 'manufacturing_date', 'has_it_completed', 'inserted_timestamp',)
 
 class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
     drone = DroneSerializer()
@@ -25,10 +25,18 @@ class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
 
 class PilotSerializer(serializers.HyperlinkedModelSerializer):
     competitions = CompetitionSerializer(many=True, read_only=True)
-    gender = serializers.ChoiceField(choises=Pilot.GENDER_CHOICES)
+    gender = serializers.ChoiceField(choices=Pilot.GENDER_CHOICES)
     gender_description = serializers.CharField(source='get_gender_display', read_only=True)
     
     
     class Meta:
         model = Pilot
         fields = ('url', 'name', 'gender', 'gender_description','races_count', 'inserted_timestamp', 'competitions', )
+
+class PilotCompetitionSerializer(serializers.ModelSerializer):
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(), slug_field='name')
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(), slug_field='name')
+
+    class Meta:
+        model = Competition
+        fields = ('url', 'pk', 'distance_in_feet', 'distance_achievement_date', 'pilot', 'drone')
--- restful01/drones/views.py
+++ restful01/drones/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics
+from drones.models import DroneCategory, Drone
+from drones.serializers import DroneCategorySerializer
+from drones.serializers import DroneSerializer
+from drones.models import Pilot, Competition
+from drones.serializers import PilotSerializer
+from drones.serializers import PilotCompetitionSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class DroneCategoryViewSet(viewsets.ModelViewSet):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = "dronecategory_list"
+
+class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = "dronecategory-detail"
+
+class DroneList(generics.ListCreateAPIView):
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = "drone_list"
+    
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = "drone-detail"
+    
+    
+
+class PilotList(generics.ListCreateAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = "pilot_list"
+
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = "pilot-detail"
+
+class CompetitionList(generics.ListCreateAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = "competition_list"
+
+class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = "competition-detail"
